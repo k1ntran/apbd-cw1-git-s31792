ad171479743bbd1af2e90d99d91ac29d0a48ac9f
Nhat Minh <s31792@pjwstk.edu.pl>
2026-02-20 05:07:28 +0100

    Integrate CalculateTotal into main program flow

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿Console.WriteLine("Welcome to the Statistics App!");
+﻿using System;
+
+Console.WriteLine("Welcome to the Statistics App!");
 int[] numbers = { 1, 2, 3, 4, 5 };
+
+// Using the helper class we created in the last commit
+int total = StatisticsHelper.CalculateTotal(numbers);
+Console.WriteLine($"The total sum is: {total}");
