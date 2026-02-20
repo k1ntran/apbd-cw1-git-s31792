b75cd06173f33fa003be3a142e583362288858ff
Nhat Minh <s31792@pjwstk.edu.pl>
2026-02-20 06:53:06 +0100

    Change welcome message on main

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the Statistics App!");
+        Console.WriteLine("Welcome to the Professional Statistics Portal!");
         Console.WriteLine("Enter some numbers separated by commas:");
         string input = Console.ReadLine();
         
